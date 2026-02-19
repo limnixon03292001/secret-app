60ac16af31045efcbfdada6ee41dbe3a9906c066
unknown <limnixon03292001@gmail.com>
2026-02-19 14:28:34 +0800

    add react-zoom-pan-pinch library

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import MapSvgIcon from "@/components/Map/MapIcon";
+import Mapv2 from "@/components/Map/Mapv2";
 import MapWrapper from "@/components/Map/MapWrapper";
 import dynamic from "next/dynamic";
 
@@ -6,7 +7,11 @@ export default function Home() {
   return (
     <>
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <MapWrapper />
+        {/* Leaflet */}
+        {/* <MapWrapper /> */}
+
+        {/* zoom pan pinch */}
+        <Mapv2 />
       </div>
     </>
   );
---
+++ components/Map/Mapv2.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+  TransformWrapper,
+  TransformComponent,
+  ReactZoomPanPinchRef,
+} from "react-zoom-pan-pinch";
+import MapSvgIcon from "./MapIcon";
+import React, { useRef } from "react";
+
+type ControlsProps = {
+  zoomIn: () => void;
+  zoomOut: () => void;
+  resetTransform: () => void;
+};
+
+const Controls = ({ zoomIn, zoomOut, resetTransform }: ControlsProps) => (
+  <>
+    <button onClick={() => zoomIn()}>+</button>
+    <button onClick={() => zoomOut()}>-</button>
+    <button onClick={() => resetTransform()}>x</button>
+  </>
+);
+
+export default function Mapv2() {
+  const transformComponentRef = useRef<ReactZoomPanPinchRef | null>(null);
+
+  const zoomToImage = () => {
+    if (transformComponentRef.current) {
+      const { zoomToElement } = transformComponentRef.current;
+      zoomToElement("imgExample");
+    }
+  };
+
+  return (
+    <TransformWrapper
+      initialScale={1}
+      initialPositionX={200}
+      initialPositionY={100}
+      ref={transformComponentRef}
+      smooth={true}
+    >
+      {(utils) => (
+        <React.Fragment>
+          <Controls {...utils} />
+          <TransformComponent
+            contentStyle={{
+              width: "100%",
+              maxWidth: "1100px",
+            }}
+          >
+            <div className="p-5">
+              <MapSvgIcon />
+            </div>
+
+            {/* <div onClick={zoomToImage}>Example text</div> */}
+          </TransformComponent>
+        </React.Fragment>
+      )}
+    </TransformWrapper>
+  );
+}
